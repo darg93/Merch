e5944a3d934748937cb4148e686f202a6f73ba61
darg93 <daniel.riano93@gmail.com>
2021-04-25 19:18:21 -0500

    añadiendo la info de los productos disponibles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () =>{
+    return (
+        <div className="Footer">
+            <p className="Footer-title">Merch App 2021</p>
+            <p className="Footer-copy">Todos los izquierdos reservados</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/containers/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import initialState from '../initialState';
+import Products from '../components/Products';
+
+const Home = () =>{
+    return (
+        <Products products={initialState.products}/>
+    );
+}
+
+export default Home;
